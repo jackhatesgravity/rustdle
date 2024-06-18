720c915791a4de9bfa85493bdaa52c5d2425c378
jackhatesgravity <18660090+jackhatesgravity@users.noreply.github.com>
2024-06-18 21:28:00 +0800

    Added print mask functionality

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 extern crate term;
 
 use std::io::{self, Write};
+use std::process::Command;
 use regex::Regex;
+use term::{StderrTerminal, StdoutTerminal};
 
 const WORD_OF_THE_DAY: &str = "flour";
 
@@ -18,24 +20,61 @@ const WORD_OF_THE_DAY: &str = "flour";
  If that's what I have to do to start off, however, then that's what I have to do.
  */
 
-fn main() {
-
+fn print_with_colour_mask(exact_mask: [u8; 5], general_mask: [u8; 5], word: &String) {
     // Create a mutable terminal for manipulating colours.
     let mut terminal = term::stdout().unwrap();
+    for n in 0..5 {
+        if exact_mask[n] == 1 {
+            terminal.fg(term::color::BRIGHT_GREEN).unwrap();
+            print!("{}", word.chars().nth(n).unwrap());
+            terminal.reset().unwrap();
+        } else if general_mask[n] == 1 {
+            terminal.fg(term::color::BRIGHT_YELLOW).unwrap();
+            print!("{}", word.chars().nth(n).unwrap());
+            terminal.reset().unwrap();
+        } else {
+            print!("{}", word.chars().nth(n).unwrap());
+        }
+    }
+    io::stdout().flush().expect("Failed to flush stdout!");
+    println!();
+}
+
+
+fn main() {
+    /*** REGEX LAND START ***/
 
     // Regex migraine time.
-    let re = Regex::new("([flour])").unwrap();
+    // let re = Regex::new("([flour])").unwrap();
+
+    // // Basic match.
+    // let mat = re.find(hay).unwrap();
+    // println!("{:?}", mat);
+
+    // // Match all instances.
+    // let matches: Vec<_> = re.find_iter(hay).map(|m| m.as_str()).collect();
+    // println!("{:?}", matches);
+
+    // // Another alternative.
+    // let mat = re.is_match(hay).unwrap();
+    // println!("{:?}", mat);
+
+    // I'm overthinking this. I can just compare like for like in a loop for the EXACT match, then use regex for the GENERAL case?
+
+    /*** REGEX LAND END ***/
 
     // Track the number of rounds.
     let mut turn = 0;
     let mut guesses: [String; 5] = Default::default(); // Initialise with empty Strings
 
     loop {
-        println!(); // Blank line at the start.
+
+        // Initialise and set masks to 0.
+        let mut exact_match_mask: [u8; 5] = Default::default(); // To mut or not to mut?
+        let mut general_match_mask: [u8; 5] = Default::default(); // Mut. Definitely mut.
+
         if turn < 5 {
-            terminal.fg(term::color::BRIGHT_GREEN).unwrap(); // I feel like this is a good target for matching.
             print!("[Round {}] Enter your guess: ", turn + 1);
-            terminal.reset().unwrap();
             io::stdout().flush().expect("Failed to flush stdout!"); // Flush to make sure we're outputting.
 
             let mut guess = String::new();
@@ -45,8 +84,29 @@ fn main() {
 
             let guess = guess.trim().to_string(); // Trim whitespace and convert to String.
 
+            /*** Here comes the fun bit involving the masks... (Ugly code warning) ***/
+            'exact: for n in 0..5 {
+                if guess.chars().nth(n).unwrap() == WORD_OF_THE_DAY.chars().nth(n).unwrap() {
+                    exact_match_mask[n] = 1;
+                }
+            }
+
+            'general: for n in 0..5 {
+                for m in 0..5 {
+                    if guess.chars().nth(n).unwrap() == WORD_OF_THE_DAY.chars().nth(m).unwrap() {
+                        general_match_mask[n] = 1;
+                    }
+                }
+            }
+
             guesses[turn] = guess.clone(); // Store the guess in the array.
             println!("Your guesses: {:?}", guesses);
+            print_with_colour_mask(exact_match_mask, general_match_mask, &guess);
+
+            // I can either order it so the exact mask takes priority, or nand the exact against the general to remove double-ups.
+
+            // println!("DEBUG::EXACT_MASK - {:?}", exact_match_mask);
+            // println!("DEBUG::GENERAL_MASK - {:?}", general_match_mask);
 
             if guess == WORD_OF_THE_DAY {
                 println!("You guessed correctly!");

59b7b0cfd1d8a4003cdc1708d2dbdc3bff857f7a
jackhatesgravity <18660090+jackhatesgravity@users.noreply.github.com>
2024-06-21 11:23:06 +0800

    Added random word functionality

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 extern crate term;
 
-use std::io::{self, Write};
+use std::{fs, vec};
+use std::io::{self, Write, Read};
+use rand::Rng;
 use term::color;
-
-const WORD_OF_THE_DAY: &str = "FLOUR";
 
 fn print_with_colour_mask(exact_mask: &[u8; 5], general_mask: &[u8; 5], word: &str) {
     if let Some(mut terminal) = term::stdout() {
@@ -15,7 +15,7 @@ fn print_with_colour_mask(exact_mask: &[u8; 5], general_mask: &[u8; 5], word: &s
                 }
                 _ => match general_mask[i] {
                     1 => {
-                        terminal.fg(color::BRIGHT_YELLOW).unwrap();
+                        terminal.fg(color::BRIGHT_MAGENTA).unwrap();
                         print!("{}", c);
                     }
                     _ => print!("{}", c),
@@ -28,8 +28,8 @@ fn print_with_colour_mask(exact_mask: &[u8; 5], general_mask: &[u8; 5], word: &s
     }
 }
 
-
-fn main() {
+fn game_loop(word: &str) {
+    let word_of_the_day: &str = word;
     let mut turn = 0; // Keep track of the number of rounds.
     let mut guesses: [String; 5] = Default::default(); // Initialise with empty Strings.
 
@@ -42,7 +42,9 @@ fn main() {
         io::stdout().flush().expect("Failed to flush stdout!"); // Flush to make sure we're outputting.
 
         let mut guess = String::new();
-        io::stdin().read_line(&mut guess).expect("Failed to read line!");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line!");
         let guess = guess.trim().to_string().to_uppercase();
 
         // Enforce word length.
@@ -51,14 +53,14 @@ fn main() {
             continue;
         }
 
-        for (i, (g, w)) in guess.chars().zip(WORD_OF_THE_DAY.chars()).enumerate() {
+        for (i, (g, w)) in guess.chars().zip(word_of_the_day.chars()).enumerate() {
             if g == w {
                 exact_match_mask[i] = 1;
             }
         }
 
         for (i, g) in guess.chars().enumerate() {
-            if WORD_OF_THE_DAY.contains(g) && exact_match_mask[i] == 0 {
+            if word_of_the_day.contains(g) && exact_match_mask[i] == 0 {
                 general_match_mask[i] = 1;
             }
         }
@@ -67,7 +69,7 @@ fn main() {
         // println!("Your guesses: {:?}", guesses);
         print_with_colour_mask(&exact_match_mask, &general_match_mask, &guess);
 
-        if guess == WORD_OF_THE_DAY {
+        if guess == word_of_the_day {
             println!("You guessed correctly!");
             break;
         } else {
@@ -76,7 +78,27 @@ fn main() {
         }
 
         if turn == 5 {
-            println!("Game over!");
+            println!("Game over! The correct word was: {word_of_the_day}");
         }
     }
+}
+
+fn read_words_from_file(file_path: &str) -> io::Result<Vec<String>> {
+    let data = fs::read_to_string(file_path)?;
+    Ok(data.lines().map(|line| line.to_string()).collect())
+}
+
+fn main() {
+
+    let words = read_words_from_file("src/words.txt").unwrap_or_else(|e| {
+        eprintln!("Error reading file: {}", e);
+        std::process::exit(1);
+    });
+
+    let mut rng = rand::thread_rng();
+    let rand_num = rng.gen_range(0..words.len());
+
+    let rand_word = &words[rand_num].to_uppercase();
+    game_loop(rand_word);
+
 }
